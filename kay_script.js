de28d56f312a1a7e0d5c427938148170940a617e
taylormari <46578643+taylormari@users.noreply.github.com>
2021-01-30 11:43:33 -0800

    Add files via upload

---
+++ kay_script.js
@@ -0,0 +1,83 @@
+$(function(){
+	$("#draggable").draggable();
+})
+
+$(function(){
+	$("#draggable1").draggable();
+})
+
+$(function(){
+	$("#draggable2").draggable();
+})
+
+$(function(){
+	$("#draggable3").draggable();
+})
+
+$(function(){
+	$("#draggable4").draggable();
+})
+
+$(function(){
+	$("#draggable5").draggable();
+})
+
+$(function(){
+	$("#draggable6").draggable();
+})
+
+$(function(){
+	$("#draggable7").draggable();
+})
+
+$(function(){
+	$("#draggable8").draggable();
+})
+
+$(function(){
+	$("#draggable9").draggable();
+})
+
+$(function(){
+	$("#draggable10").draggable();
+})
+
+$(function(){
+	$("#draggable11").draggable();
+})
+
+$(function(){
+	$("#draggable12").draggable();
+})
+
+$(function(){
+	$("#draggable13").draggable();
+})
+
+$(function(){
+	$("#draggable14").draggable();
+})
+
+$(function(){
+	$("#draggable15").draggable();
+})
+
+$(function(){
+	$("#draggable16").draggable();
+})
+
+$(function(){
+	$("#draggable17").draggable();
+})
+
+$(function(){
+	$("#draggable18").draggable();
+})
+
+$(function(){
+	$("#draggable19").draggable();
+})
+
+$(function(){
+	$("#draggable20").draggable();
+})
